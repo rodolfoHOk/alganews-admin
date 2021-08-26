5015033e6984e70fd9ec823ca6262d223440c523
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-26 14:34:50 -0300

    transform chart data

--- src/app/features/CompanyMetrics.tsx
+++ src/app/features/CompanyMetrics.tsx
@@ -1,23 +1,29 @@
 import { Line } from '@ant-design/charts';
+import { useEffect, useState } from 'react';
+import { MetricService } from 'rodolfohiok-sdk';
+import transformDataIntoAntdChart from '../../core/utils/transformDataIntoAntdChart';
 
 export default function CompanyMetrics() {
-  const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
-  ];
+  const [data, setData] = useState<
+    {
+      yearMonth: string;
+      value: number;
+      category: 'totalRevenues' | 'totalExpenses';
+    }[]
+  >([]);
+
+  useEffect(() => {
+    MetricService.getMonthlyRevenuesExpenses('2021-08')
+      .then(transformDataIntoAntdChart)
+      .then(setData);
+  }, []);
 
   const config = {
     data,
     height: 400,
-    xField: 'year',
+    xField: 'yearMonth',
     yField: 'value',
+    seriesField: 'category',
     point: {
       size: 5,
       shape: 'diamond',
