618fd9dace8a01a2755c93dcaa8469c49d6edfe6
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-27 21:38:11 -0300

    Customize rendering of table columns

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -1,7 +1,9 @@
-import { Table } from 'antd';
+import { Button, Space, Switch, Table, Tag } from 'antd';
+import { format } from 'date-fns';
 import { useEffect } from 'react';
 import { User } from 'rodolfohiok-sdk';
 import useUsers from '../../core/hooks/useUsers';
+import { EyeOutlined, EditOutlined } from '@ant-design/icons';
 
 export default function UserList() {
   const { users, fetchUsers } = useUsers();
@@ -26,18 +28,49 @@ export default function UserList() {
           {
             dataIndex: 'role',
             title: 'Perfil',
+            align: 'center',
+            render(role) {
+              return (
+                <Tag color={role === 'MANAGER' ? 'red' : 'blue'}>
+                  {role === 'EDITOR'
+                    ? 'Editor'
+                    : role === 'MANAGER'
+                    ? 'Gerente'
+                    : role === 'ASSISTANT'
+                    ? 'Assistente'
+                    : role}
+                </Tag>
+              );
+            },
           },
           {
             dataIndex: 'createdAt',
             title: 'Criação',
+            align: 'center',
+            render(createdAt: string) {
+              return format(new Date(createdAt), 'dd/MM/yyyy');
+            },
           },
           {
             dataIndex: 'active',
             title: 'Ativo',
+            align: 'center',
+            render(active) {
+              return <Switch defaultChecked={active} />;
+            },
           },
           {
             dataIndex: 'id',
             title: 'Ações',
+            align: 'center',
+            render() {
+              return (
+                <Space>
+                  <Button size="small" icon={<EyeOutlined />} />
+                  <Button size="small" icon={<EditOutlined />} />
+                </Space>
+              );
+            },
           },
         ]}
       />
