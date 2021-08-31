6e580e4ec7cf65124041b0ad7933cf315c42f142
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-30 21:50:44 -0300

    antd tabs component

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -7,7 +7,10 @@ import {
   DatePicker,
   Divider,
   Select,
+  Tabs,
 } from 'antd';
+
+const { TabPane } = Tabs;
 
 export default function UserForm() {
   return (
@@ -21,11 +24,7 @@ export default function UserForm() {
             <Input placeholder={'E.g.: João Silva'} />
           </Form.Item>
           <Form.Item label="Data de nascimento">
-            <DatePicker
-              style={{ width: '100%' }}
-              format={'DD/MM/YYYY'}
-              // allowClear
-            />
+            <DatePicker style={{ width: '100%' }} format={'DD/MM/YYYY'} />
           </Form.Item>
         </Col>
         <Col lg={10}>
@@ -49,6 +48,19 @@ export default function UserForm() {
           <Form.Item label="Email">
             <Input type="email" placeholder="E.g.: contato@joao.silva" />
           </Form.Item>
+        </Col>
+        <Col lg={24}>
+          <Divider />
+        </Col>
+        <Col lg={24}>
+          <Tabs defaultActiveKey="personal">
+            <TabPane key="personal" tab="Dados pessoais">
+              dados pessoais
+            </TabPane>
+            <TabPane key="bankAccount" tab="Dados bancários">
+              dados bancários
+            </TabPane>
+          </Tabs>
         </Col>
       </Row>
     </Form>
