e73dfe181781d2aa299f7928d363f0fdb9c8b353
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-30 22:44:16 -0300

    more personal data - Array fill map

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -9,6 +9,7 @@ import {
   Select,
   Tabs,
 } from 'antd';
+import React from 'react';
 
 const { TabPane } = Tabs;
 
@@ -86,6 +87,24 @@ export default function UserForm() {
                     <Input placeholder="0,00" />
                   </Form.Item>
                 </Col>
+                {Array(3)
+                  .fill(null)
+                  .map((_, index) => {
+                    return (
+                      <React.Fragment key={index}>
+                        <Col lg={6}>
+                          <Form.Item label="Habilidade">
+                            <Input placeholder="E.g.: Javascript" />
+                          </Form.Item>
+                        </Col>
+                        <Col lg={2}>
+                          <Form.Item label="%">
+                            <Input />
+                          </Form.Item>
+                        </Col>
+                      </React.Fragment>
+                    );
+                  })}
               </Row>
             </TabPane>
             <TabPane key="bankAccount" tab="Dados bancários">
