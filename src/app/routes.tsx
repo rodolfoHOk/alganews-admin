b8339a2b2b85890827cd0036d9a67c58d19288d4
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-31 23:54:53 -0300

    catching errors globally with onUnhandledRejection

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,7 @@
+import { message, notification } from 'antd';
+import { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import CustomError from 'rodolfohiok-sdk/dist/CustomError';
 import CashFlowExpensesView from './views/CashFlowExpenses.view';
 import CashFlowRevenuesView from './views/CashFlowRevenues.view';
 import HomeView from './views/Home.view';
@@ -8,6 +11,30 @@ import UserCreateView from './views/UserCreate.view';
 import UserListView from './views/UserList.view';
 
 export default function Routes() {
+  useEffect(() => {
+    window.onunhandledrejection = ({ reason }) => {
+      if (reason instanceof CustomError) {
+        if (reason.data?.objects) {
+          reason.data.objects.forEach((error) => {
+            message.error(error.userMessage);
+          });
+        } else {
+          notification.error({
+            message: reason.message,
+            description:
+              reason.data?.detail === 'Network reason'
+                ? 'Erro na rede'
+                : reason.data?.detail,
+          });
+        }
+      } else {
+        notification.error({
+          message: 'Houve um erro',
+        });
+      }
+    };
+  }, []);
+
   return (
     <Switch>
       <Route path={'/'} exact component={HomeView} />
