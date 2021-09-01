0b24ae1a38698c9d2879e2013570349734caaffd
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-01 20:14:45 -0300

    retrieving user for editing in URL

--- src/app/views/UserEdit.view.tsx
+++ src/app/views/UserEdit.view.tsx
@@ -1,16 +1,18 @@
 import { notification, Skeleton } from 'antd';
 import moment from 'moment';
 import { useCallback, useEffect } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import { User, UserService } from 'rodolfohiok-sdk';
 import useUser from '../../core/hooks/useUser';
 import UserForm from '../features/UserForm';
 
 export default function UserEditView() {
+  const params = useParams<{ id: string }>();
   const { user, fetchUser } = useUser();
 
   useEffect(() => {
-    fetchUser(1);
-  }, [fetchUser]);
+    if (!isNaN(Number(params.id))) fetchUser(Number(params.id));
+  }, [fetchUser, params.id]);
 
   const transformUserData = useCallback((user: User.Detailed) => {
     return {
@@ -26,6 +28,8 @@ export default function UserEditView() {
       notification.success({ message: 'Usuário foi atualizado com sucesso' });
     });
   }
+
+  if (isNaN(Number(params.id))) return <Redirect to={'/usuarios'} />;
 
   if (!user) return <Skeleton />;
 
