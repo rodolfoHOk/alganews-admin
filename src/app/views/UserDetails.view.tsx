10619d1292a604ec05bbda7a9bd67ff9afad7ce9
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-02 23:16:12 -0300

    responsiveness with hook useBreakpoint

--- src/app/views/UserDetails.view.tsx
+++ src/app/views/UserDetails.view.tsx
@@ -4,12 +4,14 @@ import {
   Card,
   Col,
   Descriptions,
+  Divider,
   Progress,
   Row,
   Skeleton,
   Space,
   Typography,
 } from 'antd';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import { useEffect } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import useUser from '../../core/hooks/useUser';
@@ -17,6 +19,8 @@ import useUser from '../../core/hooks/useUser';
 export default function UserDetailsView() {
   const params = useParams<{ id: string }>();
   const { user, fetchUser, notFound } = useUser();
+
+  const { lg } = useBreakpoint();
 
   useEffect(() => {
     if (!isNaN(Number(params.id))) fetchUser(Number(params.id));
@@ -32,16 +36,30 @@ export default function UserDetailsView() {
     <>
       <Row gutter={24}>
         <Col xs={24} lg={4}>
-          <Avatar size={120} src={user.avatarUrls.small} />
+          <Row justify="center">
+            <Avatar size={120} src={user.avatarUrls.small} />
+          </Row>
         </Col>
         <Col xs={24} lg={20}>
-          <Typography.Title level={2}>{user.name}</Typography.Title>
-          <Typography.Paragraph ellipsis>{user.bio}</Typography.Paragraph>
-          <Space>
-            <Button type="primary">Editar perfil</Button>
-            <Button type="primary">Remover</Button>
+          <Space
+            direction="vertical"
+            style={{ width: '100%' }}
+            align={lg ? 'start' : 'center'}
+          >
+            <Typography.Title level={2}>{user.name}</Typography.Title>
+            <Typography.Paragraph
+              style={{ textAlign: lg ? 'left' : 'center' }}
+              ellipsis={{ rows: 2 }}
+            >
+              {user.bio}
+            </Typography.Paragraph>
+            <Space>
+              <Button type="primary">Editar perfil</Button>
+              <Button type="primary">Remover</Button>
+            </Space>
           </Space>
         </Col>
+        <Divider />
         <Col xs={24} lg={12}>
           <Space direction="vertical" style={{ width: '100%' }}>
             {user.skills?.map((skill) => (
