be77d41afa912792bca704e08d53a51729a59175
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-06 23:51:47 -0300

    payment actions

--- src/app/views/PaymentList.view.tsx
+++ src/app/views/PaymentList.view.tsx
@@ -1,8 +1,10 @@
-import { Table, Tag } from 'antd';
+import { Button, Popconfirm, Space, Table, Tag, Tooltip } from 'antd';
 import moment from 'moment';
 import { useEffect } from 'react';
 import { Payment } from 'rodolfohiok-sdk';
 import usePayments from '../../core/hooks/usePayments';
+import { EyeOutlined, DeleteOutlined } from '@ant-design/icons';
+import confirm from 'antd/lib/modal/confirm';
 
 export default function PaymentListView() {
   const { payments, fetchPayments, fetching } = usePayments();
@@ -19,6 +21,7 @@ export default function PaymentListView() {
       <Table<Payment.Summary>
         loading={fetching}
         dataSource={payments?.content}
+        rowKey="id"
         columns={[
           {
             dataIndex: 'id',
@@ -62,6 +65,50 @@ export default function PaymentListView() {
                     ? `Aprovado em ${formattedApprovalDate}`
                     : 'Aguardando aprovação'}
                 </Tag>
+              );
+            },
+          },
+          {
+            dataIndex: 'id',
+            title: 'Ações',
+            align: 'center',
+            render(id: number, payment: Payment.Summary) {
+              return (
+                <Space>
+                  <Tooltip title="Detalhar" placement="left">
+                    <Button size="small" icon={<EyeOutlined />} />
+                  </Tooltip>
+                  <Popconfirm
+                    title="Remover agendamento?"
+                    onConfirm={() =>
+                      confirm({
+                        title: 'Remover agendamento?',
+                        content:
+                          'Esta ação é irreversível. Ao remover um agendamento, ele não poderá ser recuperado!',
+                        onOk() {
+                          console.log(
+                            'todo: implementar deleção de agendamento'
+                          );
+                        },
+                      })
+                    }
+                  >
+                    <Tooltip
+                      title={
+                        payment.canBeDeleted
+                          ? 'Remover'
+                          : 'Pagamento já aprovado'
+                      }
+                      placement="right"
+                    >
+                      <Button
+                        size="small"
+                        icon={<DeleteOutlined />}
+                        disabled={!payment.canBeDeleted}
+                      />
+                    </Tooltip>
+                  </Popconfirm>
+                </Space>
               );
             },
           },
