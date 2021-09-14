9855c19f2c2c4a3d22eac9009bfda591a46c4edb
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-14 14:32:21 -0300

    category management basis

--- src/app/features/EntryCategoryManager.tsx
+++ src/app/features/EntryCategoryManager.tsx
@@ -1,3 +1,18 @@
+import { Button, Row, Table } from 'antd';
+import { CashFlow } from 'rodolfohiok-sdk';
+
 export default function EntryCategoryManager() {
-  return <>todo: entries category manage</>;
+  return (
+    <>
+      <Row justify="space-between">
+        <Button type="primary">Atualizar categorias</Button>
+        <Button type="primary">Adicionar categoria</Button>
+      </Row>
+      <Table<CashFlow.CategorySummary>
+        dataSource={[]}
+        rowKey="id"
+        columns={[]}
+      />
+    </>
+  );
 }
--- src/app/views/CashFlowExpenses.view.tsx
+++ src/app/views/CashFlowExpenses.view.tsx
@@ -1,17 +1,32 @@
-import { Space, Typography, Tooltip, Divider, Row, Button } from 'antd';
-import { InfoCircleFilled } from '@ant-design/icons';
+import { Space, Typography, Tooltip, Divider, Row, Button, Modal } from 'antd';
+import { InfoCircleFilled, TagOutlined } from '@ant-design/icons';
 import EntriesList from '../features/EntriesList';
 import useCashFlow from '../../core/hooks/useCashFlow';
 import DoubleConfirm from '../components/DoubleConfirm';
+import { useCallback, useState } from 'react';
+import EntryCategoryManager from '../features/EntryCategoryManager';
 
 const { Title, Text } = Typography;
 
 export default function CashFlowExpensesView() {
   const { selected, fetching, removeEntriesInBatch } = useCashFlow('EXPENSE');
 
+  const [showCategoryModal, setShowCategoryModal] = useState(false);
+
+  const openCategoryModal = useCallback(() => setShowCategoryModal(true), []);
+
+  const closeCategoryModal = useCallback(() => setShowCategoryModal(false), []);
+
   return (
     <>
-      <Row>
+      <Modal
+        closeIcon={<></>}
+        visible={showCategoryModal}
+        onCancel={closeCategoryModal}
+      >
+        <EntryCategoryManager />
+      </Modal>
+      <Row justify="space-between" style={{ marginBottom: 16 }}>
         <DoubleConfirm
           popConfirmTitle={`Remover ${
             selected.length > 1
@@ -28,6 +43,13 @@ export default function CashFlowExpensesView() {
             Remover
           </Button>
         </DoubleConfirm>
+        <Button
+          type="primary"
+          icon={<TagOutlined />}
+          onClick={openCategoryModal}
+        >
+          Categorias
+        </Button>
       </Row>
       <Space direction="vertical">
         <Title level={3}>Recuperando entradas do mês de agosto</Title>
