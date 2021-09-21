ee765d164b111bd440962f6f906f42103aaae9df
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-21 20:30:02 -0300

    checking registry updates

--- src/app/features/EntryDetails.tsx
+++ src/app/features/EntryDetails.tsx
@@ -44,8 +44,21 @@ export default function EntryDetails({ entryId }: EntryDetailsProps) {
         {formatToBrl(entry?.amount)}
       </Descriptions.Item>
       <Descriptions.Item label="Criado em">
-        {moment(entry?.createdAt).format('DD/MM/YYYY')}
+        {moment(entry?.createdAt).format('DD/MM/YYYY \\à\\s HH:mm')}
       </Descriptions.Item>
+      <Descriptions.Item label="Criado por">
+        {entry?.createdBy.name}
+      </Descriptions.Item>
+      {entry?.createdAt !== entry?.updatedAt && (
+        <>
+          <Descriptions.Item label="Atualizado em">
+            {moment(entry?.updatedAt).format('DD/MM/YYYY \\à\\s HH:mm')}
+          </Descriptions.Item>
+          <Descriptions.Item label="Atualizado por">
+            {entry?.updatedBy.name}
+          </Descriptions.Item>
+        </>
+      )}
     </Descriptions>
   );
 }
