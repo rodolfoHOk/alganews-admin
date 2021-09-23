c512e51e76d8c8cd12ccb2ecddd894921c28dc5f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-23 20:44:07 -0300

    profile dropdown

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -1,4 +1,6 @@
-import { Avatar, Layout, Row } from 'antd';
+import { Avatar, Layout, Row, Dropdown, Menu, Card, Tag } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
 import logo from '../../../assets/logo.svg';
 import useAuth from '../../../core/hooks/useAuth';
 
@@ -16,7 +18,36 @@ export default function DefaultLayoutHeader() {
         style={{ height: '100%', maxWidth: 1190, margin: '0 auto' }}
       >
         <img src={logo} alt="AlgaNews Admin" />
-        <Avatar src={user?.avatarUrls.small} />
+        <Dropdown
+          placement="bottomRight"
+          overlay={
+            <Card style={{ width: 220 }}>
+              <Card bordered={false}>
+                <Meta
+                  // avatar={<Avatar src={user?.avatarUrls.small} />}
+                  title={user?.name}
+                  description={
+                    <Tag color={user?.role === 'MANAGER' ? 'red' : 'blue'}>
+                      {user?.role === 'MANAGER'
+                        ? 'Gerente'
+                        : user?.role === 'ASSISTANT'
+                        ? 'Assistente'
+                        : 'Editor'}
+                    </Tag>
+                  }
+                />
+              </Card>
+              <Menu>
+                <Menu.Item icon={<UserOutlined />}>Meu Perfil</Menu.Item>
+                <Menu.Item icon={<LogoutOutlined />} danger>
+                  Fazer Logout
+                </Menu.Item>
+              </Menu>
+            </Card>
+          }
+        >
+          <Avatar src={user?.avatarUrls.small} />
+        </Dropdown>
       </Row>
     </Header>
   );
