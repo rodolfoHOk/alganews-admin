46a33aac594370b951ee2e68c2fdea164e1047a4
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-23 21:21:13 -0300

    disabling functions by permissions

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -222,6 +222,10 @@ export default function UserList() {
             render(active, user) {
               return (
                 <Switch
+                  disabled={
+                    (active && !user.canBeDeactivated) ||
+                    (!active && !user.canBeActivated)
+                  }
                   checked={active}
                   onChange={() => toggleUserStatus(user)}
                 />
